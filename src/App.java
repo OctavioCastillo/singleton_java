95a755a3a97b65c51a7071860faee9dc8c6c3b65
Octavio Castillo <luisoctaviocs02@gmail.com>
2024-06-12 15:46:31 -0600

    singleton commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        A a1 = A.getInstance();
+        A a2 = A.getInstance();
+
+        System.out.println(a1);
+        System.out.println(a2);
+    }
+}
